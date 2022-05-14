b324d1201b6edac94d2da77b095892d8c457100d
jjiajun <teo.jia.jun.29@gmail.com>
2022-05-15 03:34:40 +0800

    removed commented out code

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,10 @@ import helmet from 'helmet';
 import dotenv from "dotenv";
 dotenv.config();
 const { PORT } = process.env;
+
 const app = express();
 
-app.use(helmet())
+app.use(helmet()) // protects the app from well-known web vulnerabilities by setting up HTTP headers appropriately
 
 app.use(express.json()) // allows you to parse json body, if not the json in req.body will not be detected
 
@@ -15,29 +16,3 @@ routes(app);
 app.listen(PORT, () => {
   console.log(`Application listening at http://localhost:${PORT}`)
 })
-
-
-
-
-
-
-// app.use(express.urlencoded({ extended: true })) // allows you to parse URL
-
-// const middleware = ({name}:{name:string}) => (req: Request, res: Response, next: NextFunction) => {
-//   res.locals.name = name;
-//   next();
-// }
-
-// app.use(middleware({name: "Tom does tech"}));
-
-// app.all('/api/all', (req: Request, res: Response) => {
-//   return res.sendStatus(200)
-// })
-
-// app.route("/").get((req: Request, res: Response) => {
-//   return res.send("You make a GET request")
-// }).post((req: Request, res: Response) => {
-//   return res.send("You made a POST request")
-// }).delete((req: Request, res: Response) => {
-//   return res.send("You made a DELETE request")
-// })
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,6 @@
 import {Express} from 'express';
 import readTransactions from "./controllers/readTransactions";
 import addTransaction from "./controllers/addTransaction";
-// when do I use express.Router()?
 
 const routes = (app: Express) => {
   app.get('/transactions', readTransactions);
